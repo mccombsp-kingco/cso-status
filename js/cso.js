c963bfad61379b932662da19165bfdbef34575f2
mccombsp-kingco <mccombs@imperium.org>
2015-02-12 14:47:15 -0800

    popups

--- js/cso.js
+++ js/cso.js
@@ -9,14 +9,14 @@ $(document).ready(function() {
         .loadURL('/data_display/cso_test_file.geojson')
        .addTo(map);
 
-    featureLayer.on('ready', function(layer, feature) {
+    featureLayer.on('ready', function(layer) {
 
     // here you call `bindPopup` with a string of HTML you create - the feature
     // properties declared in geoJSON file are available under `layer.feature.properties`
 
-    var content = '<strong>'+.feature.properties.CSO_TagName+' '+feature.properties.DSN+
-              ' '+feature.properties.Name+'</strong>'+
-              feature.properties.description+': '+feature.properties.Time_stamp;
+    var content = '<strong>'+layer.feature.properties.CSO_TagName+' '+layer.feature.properties.DSN+
+              ' '+layer.feature.properties.Name+'</strong>'+
+              layer.feature.properties.description+': '+layer.feature.properties.Time_stamp;
 
     layer.bindPopup(content)
  });
