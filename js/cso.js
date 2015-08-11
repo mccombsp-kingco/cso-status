216ccc17704fbd016fe410cb858ba8f50cb7bb58
keum <keum.peter@gmail.com>
2015-08-11 14:39:35 -0700

    change basemap

--- js/cso.js
+++ js/cso.js
@@ -54,8 +54,8 @@ $(document).ready(function() {
     //var tileLayer = L.tileLayer('/gisdata/tiles/langley-2nd-street-2014/{z}/{x}/{y}.png', {foo: 'bar', tms: true, minZoom:1, maxZoom:22, bounds:bounds}).addTo(map);
 
     layerControl = L.control.layers({
-        'Base Map': L.mapbox.tileLayer('examples.map-i87786ca').addTo(map),
-        'Grey Map': L.mapbox.tileLayer('examples.map-20v6611k')
+        'Base Map': L.mapbox.tileLayer('mapbox.streets').addTo(map),
+        'Satellite Map': L.mapbox.tileLayer('mapbox.satellite')
     }, {
        // "Quadcopter Stitch": tileLayer,
         "CSO Status": featureLayer
